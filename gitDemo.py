293afd73cf94c86d3b2cd234401483578b60972f
Vic Brown <vicbrown@Vics-MacBook-Air.local>
2019-07-29 10:25:39 -0700

    Added gitDemo.py

---
+++ gitDemo.py
@@ -0,0 +1,16 @@
+# -------------------------------------------------
+#        Name: <VIC BROWN>
+#    Filename: gitDemo.py
+#        Date: <July 29, 2019>
+#
+# Description: <This is a sample python file, including a header
+#                and main function>
+# -------------------------------------------------
+
+def main():
+
+    name = "Vic"
+    print("Hello, my name is", name)
+
+if __name__ == "__main__":
+    main()
